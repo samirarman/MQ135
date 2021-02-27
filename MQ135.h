2a168e065a3ff8639471da8c6a903194c0b5e9ae
Samir Arman <samir.arman@gmail.com>
2021-02-27 00:20:57 -0300

    Update MQ135.h

--- MQ135.h
+++ MQ135.h
@@ -24,16 +24,11 @@ v1.0 - First release
 /// The load resistance on the board
 #define RLOAD 10.0
 /// Calibration resistance at atmospheric CO2 level
-#define RZERO 76.63
+#define RZERO 156.696997448
+
 /// Parameters for calculating ppm of CO2 from sensor resistance
 #define PARA 116.6020682
 #define PARB 2.769034857
-
-/// Parameters to model temperature and humidity dependence
-#define CORA 0.00035
-#define CORB 0.02718
-#define CORC 1.39538
-#define CORD 0.0018
 
 /// Atmospheric CO2 level for calibration purposes
 #define ATMOCO2 397.13
